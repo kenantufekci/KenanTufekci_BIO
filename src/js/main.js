d73fef6b55af79b8f87f54bd1f6f0a8652de211e
Kenan Tufekci <kenantt@ufl.edu>
2015-09-04 04:13:24 -0400

    Added some features

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('GreetingController', ['$scope', function($scope) {
-    $scope.greeting = 'Hola!';
+myApp.controller('DoubleController', ['$scope', function($scope) {
+    $scope.double = function(value) { return value * 2; };
 }]);
